a7eb72a22b42e69702eb6c85936512e3d092458e
pat-moreno <pat.gb.moreno@gmail.com>
2025-06-21 11:30:02 -0300

    vieja configuracion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,66 @@
+-- ## opts ## --
+vim.g.mapleader = " "
 vim.cmd("colorscheme retrobox")
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+
+vim.opt.wrap = false 
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 500
+
+vim.opt.completeopt = "menu,menuone,noinsert"
+
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+-- ## remap options ##
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+
+-- ## plugin manager ##
+
+
