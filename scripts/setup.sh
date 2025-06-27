2f3e0ad497dacea21109df285191add5bd4f7655
pat-moreno <pat.gb.moreno@gmail.com>
2025-06-27 06:43:16 -0300

    stow y auto setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+git clone https://github.com/pat-moreno/dot && cd dot && stow --adopt --ignore='^scripts$' . && cd ~
+
+mkdir -r personal/builds && cd personal/builds
+
+git clone git://git.suckless.org/dmenu
+cd dmenu && cp config.def.h config.h && sed -i '7s/monospace:size=10/JetBrains Mono:size=12/' config.h && cd ..
+git clone git://git.suckless.org/st
+cd st && cp config.def.h config.h && sed -i '8s/Liberation Mono:pixelsize=12/JetBrains Mono:pixelsize=20/' config.h && cd ..
+git clone git://git.suckless.org/dwm
+cd dwm && cp config.def.h config.h && sed -i 's/monospace:size=10/JetBrains Mono:size=12/g' -e '30s/Gimp/NULL/' -e '30s/1/False/' -e '31d' -e '47s/Mod1Mask/Mod4Mask/' config.h && cd ..
+
+pacman -S xorg-server xorg-xinit libx11 libxinerama libxft libxrandr xf86-video-intel mesa ttf-jetbrains-mono noto-fonts-cjk 
+
+cd dmenu && sudo make clean install 
+
+cd ../st && sudo make clean install
+
+cd ../dwm && sudo make clean install && cd ~
+
+
