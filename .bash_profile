1f78f52e17ad53d2e4caff1d8afc611621e25e35
pat-moreno <miscelaneo.patricio@gmail.com>
2026-03-01 14:05:08 -0300

    reinstall

--- .bash_profile
+++ .bash_profile
@@ -6,5 +6,3 @@
 
 export PATH="${PATH}:/home/patricio/personal/bin"
 
-#export XDG_DATA_HOME="$HOME/.local/share"
-#export XDG_DATA_DIRS="/usr/share/dbus-1/services"
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,7 +57,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<leader>fb", ":w<CR>:!fbc % && ./%<<CR>")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
 
 -- ## plugin manager ##
 --[[
