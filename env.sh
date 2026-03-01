d044b7768c3b0dacc7b8d2012066c7bd09f08ac5
pat-moreno <pat.gb.moreno@gmail.com>
2026-03-01 16:37:04 -0300

    env

--- env.sh
+++ env.sh
@@ -5,14 +5,19 @@ mkdir -p $HOME/personal/bin
 export BUILDS="$HOME/personal/builds"
 mkdir -p $HOME/.config/nvim 
 
-sudo pacman -S git
+sudo -v
 
 git clone git://git.suckless.org/dmenu $BUILDS/dmenu
 git clone git://git.suckless.org/st $BUILDS/st
 git clone git://git.suckless.org/dwm $BUILDS/dwm
 
-git clone https://github.com/pat-moreno/dot $HOME/dot
+
+cd $HOME 
+
+sudo pacman -S neovim xclip cmus mpv ffmpeg alsa-utils xorg-server xorg-xinit xf86-video-intel noto-fonts-cjk noto-fonts pass gnupg openssh yt-dlp stow tmux libxinerama --noconfirm
+
 rm .bashrc
+rm .bash_profile
 cd $HOME/dot && stow .
 
 cd $BUILDS/st && cp config.def.h config.h
@@ -22,12 +27,10 @@ patch < patch-dwm.diff
 cd $BUILDS/dmenu && cp config.def.h config.h
 patch < patch-dmenu.diff
 
-cd $HOME 
-sudo pacman -S libxinerama --noconfirm
+cd $HOME
 
-sudo -v
 cd $BUILDS/dmenu && sudo make clean install 
 cd $BUILDS/st && sudo make clean install
 cd $BUILDS/dwm && sudo make clean install
 
-sudo pacman -S neovim xclip cmus mpv ffmpeg alsa-utils qutebrowser xorg-server xorg-xinit xf86-video-intel noto-fonts-cjk noto-fonts pass gnupg openssh yt-dlp stow man-db tmux --noconfirm
+sudo -K
